35df9c05c96a2c95ba285957e65ab2a0a1d08a60
xhe17 <xhe17@stanford.edu>
2020-08-06 01:53:16 -0700

    unit test code style fix

--- tests/utils.py
+++ tests/utils.py
@@ -7,16 +7,22 @@ np.random.seed(0)
 
 def simple_networkx_graph(directed=True):
     num_nodes = 10
-    # node = np.arange(num_nodes).astype(np.int64)
-    edge_index = \
-        torch.tensor([[0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 7, 9],
-                      [1, 2, 2, 3, 3, 8, 4, 5, 6, 5, 6, 7, 8, 9, 8, 9, 8]]).long()
+    edge_index = (
+        torch.tensor(
+            [
+                [0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 7, 9],
+                [1, 2, 2, 3, 3, 8, 4, 5, 6, 5, 6, 7, 8, 9, 8, 9, 8]
+            ]
+        ).long()
+    )
     x = torch.zeros([num_nodes, 2])
     y = torch.tensor([0, 0, 1, 1, 2, 2, 3, 3, 4, 4]).long()
     for i in range(num_nodes):
         x[i] = np.random.randint(1, num_nodes)
     edge_x = torch.zeros([edge_index.shape[1], 2])
-    edge_y = torch.tensor([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3]).long()
+    edge_y = torch.tensor(
+        [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3]
+    ).long()
     for i in range(edge_index.shape[1]):
         edge_x[i] = np.random.randint(1, num_nodes)
 
@@ -33,7 +39,9 @@ def simple_networkx_graph(directed=True):
         edge_x = np.zeros([edge_x.shape[0] * 2, edge_x.shape[1]])
         edge_y = np.zeros(edge_y.shape[0] * 2).astype(np.int64)
         for i, nx_edge in enumerate(nx.to_edgelist(H)):
-            edge_index[:, i] = np.array([nx_edge[0], nx_edge[1]]).astype(np.int64)
+            edge_index[:, i] = (
+                np.array([nx_edge[0], nx_edge[1]]).astype(np.int64)
+            )
             edge_x[i] = nx_edge[2]['edge_attr']
             edge_y[i] = nx_edge[2]['edge_y']
 
@@ -71,22 +79,42 @@ def generate_simple_hete_graph(no_edge_type=False):
             node_feature = torch.rand([10, ])
             node_type = "n1"
             node_label = 0
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
-        elif i >=2 and i < 4:
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature
+            )
+        elif 2 <= i < 4:
             node_feature = torch.rand([12, ])
             node_type = "n2"
             node_label = 0
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
-        elif i >=4 and i < 6:
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature
+            )
+        elif 4 <= i < 6:
             node_feature = torch.rand([10, ])
             node_type = "n1"
             node_label = 1
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature
+            )
         else:
             node_feature = torch.rand([12, ])
             node_type = "n2"
             node_label = 1
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature
+            )
     if no_edge_type:
         G.add_edge(0, 1, edge_label=0, edge_feature=torch.rand([8, ]))
         G.add_edge(0, 2, edge_label=1, edge_feature=torch.rand([8, ]))
@@ -105,22 +133,54 @@ def generate_simple_hete_graph(no_edge_type=False):
         G.add_edge(8, 0, edge_label=0, edge_feature=torch.rand([8, ]))
         G.add_edge(8, 1, edge_label=0, edge_feature=torch.rand([8, ]))
     else:
-        G.add_edge(0, 1, edge_label=0, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(0, 2, edge_label=1, edge_feature=torch.rand([12, ]), edge_type="e2")
-        G.add_edge(0, 5, edge_label=0, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(1, 3, edge_label=0, edge_feature=torch.rand([12, ]), edge_type="e2")
-        G.add_edge(1, 5, edge_label=1, edge_feature=torch.rand([12, ]), edge_type="e2")
-        G.add_edge(2, 3, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(2, 4, edge_label=2, edge_feature=torch.rand([12, ]), edge_type="e2")
-        G.add_edge(3, 4, edge_label=2, edge_feature=torch.rand([12, ]), edge_type="e2")
-        G.add_edge(4, 0, edge_label=1, edge_feature=torch.rand([12, ]), edge_type="e2")
-        G.add_edge(4, 5, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(5, 7, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(6, 1, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(6, 2, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(7, 3, edge_label=2, edge_feature=torch.rand([8, ]), edge_type="e1")
-        G.add_edge(8, 0, edge_label=0, edge_feature=torch.rand([12, ]), edge_type="e2")
-        G.add_edge(8, 1, edge_label=0, edge_feature=torch.rand([12, ]), edge_type="e2")
+        G.add_edge(
+            0, 1, edge_label=0, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            0, 2, edge_label=1, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
+        G.add_edge(
+            0, 5, edge_label=0, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            1, 3, edge_label=0, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
+        G.add_edge(
+            1, 5, edge_label=1, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
+        G.add_edge(
+            2, 3, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            2, 4, edge_label=2, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
+        G.add_edge(
+            3, 4, edge_label=2, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
+        G.add_edge(
+            4, 0, edge_label=1, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
+        G.add_edge(
+            4, 5, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            5, 7, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            6, 1, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            6, 2, edge_label=1, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            7, 3, edge_label=2, edge_feature=torch.rand([8, ]), edge_type="e1"
+        )
+        G.add_edge(
+            8, 0, edge_label=0, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
+        G.add_edge(
+            8, 1, edge_label=0, edge_feature=torch.rand([12, ]), edge_type="e2"
+        )
     return G
 
 
@@ -131,22 +191,42 @@ def generate_simple_hete_dataset(no_edge_type=False):
             node_feature = torch.rand([10, ])
             node_type = "n1"
             node_label = 0
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
-        elif i >= 2 and i < 4:
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
+        elif 2 <= i < 4:
             node_feature = torch.rand([12, ])
             node_type = "n2"
             node_label = 0
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
-        elif i >= 4 and i < 6:
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
+        elif 4 <= i < 6:
             node_feature = torch.rand([10, ])
             node_type = "n1"
             node_label = 1
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
         else:
             node_feature = torch.rand([12, ])
             node_type = "n2"
             node_label = 1
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
     if no_edge_type:
         G.add_edge(0, 1, edge_feature=torch.rand([8, ]))
         G.add_edge(0, 2, edge_feature=torch.rand([8, ]))
@@ -192,12 +272,22 @@ def generate_dense_hete_graph(no_edge_type=False):
             node_feature = torch.rand([10, ])
             node_type = "n1"
             node_label = 0
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
         else:
             node_feature = torch.rand([12, ])
             node_type = "n2"
             node_label = 1
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
 
     if no_edge_type:
         for i, j in itertools.permutations(range(num_node), 2):
@@ -214,9 +304,21 @@ def generate_dense_hete_graph(no_edge_type=False):
             if (rand > 0.8):
                 continue
             elif rand > 0.4:
-                G.add_edge(i, j, edge_label=0, edge_feature=torch.rand([8, ]), edge_type='e1')
+                G.add_edge(
+                    i,
+                    j,
+                    edge_label=0,
+                    edge_feature=torch.rand([8, ]),
+                    edge_type='e1',
+                )
             else:
-                G.add_edge(i, j, edge_label=0, edge_feature=torch.rand([8, ]), edge_type='e2')
+                G.add_edge(
+                    i,
+                    j,
+                    edge_label=0,
+                    edge_feature=torch.rand([8, ]),
+                    edge_type='e2',
+                )
     return G
 
 
@@ -228,12 +330,22 @@ def generate_dense_hete_dataset(no_edge_type=False):
             node_feature = torch.rand([1, ])
             node_type = "n1"
             node_label = 0
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
         else:
             node_feature = torch.rand([1, ])
             node_type = "n2"
             node_label = 1
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
 
     if no_edge_type:
         for i, j in itertools.permutations(range(num_node), 2):
@@ -250,9 +362,19 @@ def generate_dense_hete_dataset(no_edge_type=False):
             if (rand > 0.8):
                 continue
             elif rand > 0.4:
-                G.add_edge(i, j, edge_feature=torch.rand([1, ]), edge_type='e1')
+                G.add_edge(
+                    i,
+                    j,
+                    edge_feature=torch.rand([1, ]),
+                    edge_type='e1',
+                )
             else:
-                G.add_edge(i, j, edge_feature=torch.rand([1, ]), edge_type='e2')
+                G.add_edge(
+                    i,
+                    j,
+                    edge_feature=torch.rand([1, ]),
+                    edge_type='e2',
+                )
     return G
 
 
@@ -264,12 +386,22 @@ def generate_dense_hete_multigraph(no_edge_type=False):
             node_feature = torch.rand([10, ])
             node_type = "n1"
             node_label = 0
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
         else:
             node_feature = torch.rand([12, ])
             node_type = "n2"
             node_label = 1
-            G.add_node(i, node_type=node_type, node_label=node_label, node_feature=node_feature)
+            G.add_node(
+                i,
+                node_type=node_type,
+                node_label=node_label,
+                node_feature=node_feature,
+            )
 
     if no_edge_type:
         for i, j in itertools.permutations(range(num_node), 2):
@@ -288,9 +420,33 @@ def generate_dense_hete_multigraph(no_edge_type=False):
             if (rand > 0.8):
                 continue
             elif rand > 0.4:
-                G.add_edge(i, j, edge_label=0, edge_feature=torch.rand([8, ]), edge_type='e1')
-                G.add_edge(i, j, edge_label=0, edge_feature=torch.rand([8, ]), edge_type='e1')
+                G.add_edge(
+                    i,
+                    j,
+                    edge_label=0,
+                    edge_feature=torch.rand([8, ]),
+                    edge_type='e1',
+                )
+                G.add_edge(
+                    i,
+                    j,
+                    edge_label=0,
+                    edge_feature=torch.rand([8, ]),
+                    edge_type='e1',
+                )
             else:
-                G.add_edge(i, j, edge_label=0, edge_feature=torch.rand([8, ]), edge_type='e2')
-                G.add_edge(i, j, edge_label=0, edge_feature=torch.rand([8, ]), edge_type='e2')
+                G.add_edge(
+                    i,
+                    j,
+                    edge_label=0,
+                    edge_feature=torch.rand([8, ]),
+                    edge_type='e2',
+                )
+                G.add_edge(
+                    i,
+                    j,
+                    edge_label=0,
+                    edge_feature=torch.rand([8, ]),
+                    edge_type='e2',
+                )
     return G
