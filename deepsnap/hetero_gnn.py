26a650f94b2b46d6cd032666e775b18ad9e11574
Zecheng Zhang <zechengzhang97@gmail.com>
2021-02-24 00:50:32 -0800

    Modify hetero_gnn forward_op

--- deepsnap/hetero_gnn.py
+++ deepsnap/hetero_gnn.py
@@ -189,8 +189,10 @@ def forward_op(x, func, **kwargs):
     """
     if not isinstance(x, dict):
         raise ValueError("The input x should be a dictionary")
+    if not isinstance(func, dict):
+        raise ValueError("The input func should be a dictionary")
     for key in x:
-        x[key] = func(x[key], **kwargs)
+        x[key] = func[key](x[key], **kwargs)
     return x
 
 
