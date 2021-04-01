54c454b2d28bb9403fda1b3f6372a00511359740
Zecheng Zhang <zechengzhang97@gmail.com>
2021-04-01 01:08:23 -0700

    Add deepsnap.set_seed

--- deepsnap/__init__.py
+++ deepsnap/__init__.py
@@ -1,3 +1,7 @@
+import types
+import torch
+import random
+import numpy as np
 import deepsnap.graph
 import deepsnap.dataset
 import deepsnap.batch
@@ -9,12 +13,26 @@ import networkx as _netlib
 __version__ = "0.2.0"
 
 def use(netlib=None):
-    r"""Specifying to use which graph library as the DeepSNAP graph backend.
+    r"""Specifies to use which graph library as the DeepSNAP backend.
 
     Args:
-        netlib (module): The module of the graph library.
+        netlib (types.ModuleType): The module of the graph library.
 
     """
     global _netlib
     if netlib is not None:
         _netlib = netlib
+
+def set_seed(seed):
+	r"""
+	Sets seeds to generate random numbers. This function will set seeds for :obj:`random`, 
+	:obj:`numpy.random`, :obj:`torch.manual_seed`, and :obj:`torch.cuda.manual_seed_all`.
+
+    Args:
+        seed (int): The seed value to generate random numbers.
+
+    """
+	random.seed(seed)
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed)
